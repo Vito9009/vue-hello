752088054563b8f399bccb48ab646b97504c339b
Your Name <yan.vi193@gmail.com>
2022-01-13 14:02:12 +0100

    esercizio-ok

---
+++ js/myscript.js
@@ -0,0 +1,13 @@
+/*Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
+
+let app = new Vue({
+    el: '#container',
+    data:{
+        message: 'Ciao VUE. Benvenuto!',
+        myImg: 'https://icare-cro.com/blog/wp-content/uploads/2017/07/benvenuto.jpg'
+    }
+})
